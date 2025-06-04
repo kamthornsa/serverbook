e4215fca135f13040e0123a9c2125cbd4b5a9154
kamthorn <kamthorn.sa@ksu.ac.th>
2025-06-04 16:10:07 +0700

    first commit

---
+++ routes/authors.js
@@ -0,0 +1,41 @@
+import express from "express"
+import pool from "../db/db.js"
+
+const router = express.Router()
+
+// อ่านข้อมูลจาก Table Authors
+router.get('/', async (req , res) => {
+  try {
+    const result = await pool.query("SELECT * FROM authors")
+    if(result.rows.length === 0) {
+      return res.json ({
+        msg: "ไม่พบข้อมูลผู้เขียน"
+      })
+    }
+    res.json( result.rows)
+  } catch (err) {
+    res.status(500).json({
+      error : "Server Error"
+    })
+  }
+})
+
+// อ่านข้อมูลจาก Table Authors by ID
+router.get('/:id', async (req , res) => {
+  const { id } = req.params
+  try {
+    const result = await pool.query("SELECT * FROM authors WHERE author_id = $1", [id])
+    if(result.rows.length === 0) {
+      return res.json ({
+        msg: "ไม่พบข้อมูลผู้เขียน"
+      })
+    }
+    res.json( result.rows)
+  } catch (err) {
+    res.status(500).json({
+      error : "Server Error"
+    })
+  }
+})
+
+export default router
